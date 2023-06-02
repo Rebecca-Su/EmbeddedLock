ee0c6abe1822b7aa43f9e03bbca8d73d29b5d65c
Rebecca-Su <41730355+Rebecca-Su@users.noreply.github.com>
2023-06-02 03:05:06 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,10 +1,3 @@
-/**
-   Arduino Calculator
-
-   Copyright (C) 2020, Uri Shaked.
-   Released under the MIT License.
-*/
-
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 #include <Servo.h>
@@ -13,7 +6,7 @@ const byte KEYPAD_ROWS = 4;
 const byte KEYPAD_COLS = 3;
 const uint16_t NUMBER_OF_TRIES_MAX = 3;
 const uint16_t NUMBER_OF_DIGITS = 4;
-const String PASSWORD = "1234";
+const uint16_t TIMEOUT = 30;
 
 /* Display */
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
@@ -30,8 +23,6 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
 
-uint64_t value = 0;
-
 /* Enter password */
 void showDefaultScreen() {
   lcd.print("Enter Password");
@@ -41,127 +32,79 @@ void showDefaultScreen() {
   lcd.print(message);
 }
 
-void updateCursor() {
-  if (millis() / 250 % 2 == 0 ) {
-    lcd.cursor();
-  } else {
-    lcd.noCursor();
-  }
+void showSetupScreen() {
+  lcd.print("Setup (Max 16)");
+  lcd.setCursor(0, 1);
+  lcd.print("#=Redo *=Confirm");
 }
 
 void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
 
-  showDefaultScreen();
-  // lcd.noCursor();
-  // lcd.clear();
-  // lcd.cursor();
-
-  // lcd.setCursor(1, 0);
+  showSetupScreen();
 }
 
-
-char operation = 0;
-String memory = "";
 String current = "";
-uint64_t currentDecimal;
-bool decimalPoint = false;
-uint16_t NUMBER_OF_TRIES = 0;
-
-double calculate(char operation, double left, double right) {
-  switch (operation) {
-    case '+': return left + right;
-    case '-': return left - right;
-    case '*': return left * right;
-    case '/': return left / right;
-  }
-}
-
-void processInputOld(char key) {
-  if ('-' == key && current == "") {
-    current = "-";
-    lcd.print("-");
-    return;
-  }
-
-  switch (key) {
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-      if (!operation) {
-        memory = current;
-        current = "";
-      }
-      operation = key;
-      lcd.setCursor(0, 1);
-      lcd.print(key);
-      lcd.setCursor(current.length() + 1, 1);
-      return;
-
-    case '=':
-      float leftNum = memory.toDouble();
-      float rightNum = current.toDouble();
-      memory = String(calculate(operation, leftNum, rightNum));
-      current = "";
-      lcd.clear();
-      lcd.setCursor(1, 0);
-      lcd.print(memory);
-      lcd.setCursor(0, 1);
-      lcd.print(operation);
-      return;
-
-  }
-
-  if ('.' == key && current.indexOf('.') >= 0) {
-    return;
-  }
-
-  if ('.' != key && current == "0") {
-    current = String(key);
-  } else if (key) {
-    current += String(key);
-  }
-
-  lcd.print(key);
-}
+String PASSWORD = "";
+uint16_t NUMBER_OF_TRIES = 1;
+bool isFirstTime = true;
 
 void processInput(char key) {
   if (current == "") {
     lcd.clear();
   }
-  lcd.print(key);
-  
-  current += String(key);
 
-  if (current.length() >= 4) {
-    lcd.clear();
-    if (current == PASSWORD) {
-      NUMBER_OF_TRIES = 0;
+  if (key != '*' && key != '#') {
+    lcd.print(key);
+    current += String(key);
+  }
+
+  if (isFirstTime) {
+    if (key == '#') {
       current = "";
-      /* This will be replaced by the actual unlock of the lock*/ 
-      lcd.print("Unlock");
-      lcd.setCursor(0, 1);
-      lcd.print("Successful");
-    } else if (NUMBER_OF_TRIES < NUMBER_OF_TRIES_MAX) {
-      NUMBER_OF_TRIES ++;
+      lcd.clear();
+      showSetupScreen();
+    } else if (key == '*') {
+      isFirstTime = false;
+      PASSWORD = current;
       current = "";
-      lcd.print("Wrong Password");
-    } else {
-      NUMBER_OF_TRIES = 0;
-      current = "";
-      lcd.print("Access Denied");
-      lcd.setCursor(0, 1);
-      lcd.print("Blocked for 30 seconds");
-      delay(30000);
+      lcd.clear();
+      showDefaultScreen();
+    }
+  } else {
+    if (current.length() >= PASSWORD.length()) {
+      lcd.clear();
+      if (current == PASSWORD) {
+        NUMBER_OF_TRIES = 1;
+        current = "";
+        /* This will be replaced by the actual unlock of the lock*/ 
+        lcd.print("Unlock");
+        lcd.setCursor(0, 1);
+        lcd.print("Successful");
+      } else if (NUMBER_OF_TRIES < NUMBER_OF_TRIES_MAX) {
+        NUMBER_OF_TRIES ++;
+        current = "";
+        lcd.print("Wrong Password");
+      } else {
+        NUMBER_OF_TRIES = 1;
+        current = "";
+
+        lcd.print("Access Denied");
+        lcd.setCursor(0, 1);
+        String message = "Blocked for # seconds";
+        message.replace("#", String(TIMEOUT));
+        lcd.print(message);
+        delay(1000 * TIMEOUT);
+
+        lcd.clear();
+        showDefaultScreen();
+      }
     }
   }
 }
 
 void loop() {
-  // updateCursor();
-
   char key = keypad.getKey();
   if (key) {
     processInput(key);
