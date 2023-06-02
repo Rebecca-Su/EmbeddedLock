9e5c52b6ea23c3ced26886a7f50a471f279c0202
Rebecca-Su <41730355+Rebecca-Su@users.noreply.github.com>
2023-06-02 04:36:02 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -24,10 +24,13 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_C
 
 String current = "";
 String password = "";
+String pin = "";
 uint16_t numberOfTries = 1;
-bool isFirstTime = true;
+bool isSetPassword = true;
+bool isSetPIN = true;
+bool isEnterPIN = false;
 
-void showDefaultScreen() {
+void showEnterPasswordScreen() {
   lcd.print("Enter Password");
   lcd.setCursor(0, 1);
   String message = "(# Digits)";
@@ -35,8 +38,20 @@ void showDefaultScreen() {
   lcd.print(message);
 }
 
-void showSetupScreen() {
-  lcd.print("Setup (Max 16)");
+void showEnterPINScreen() {
+  lcd.print("Enter PIN");
+  lcd.setCursor(0, 1);
+  lcd.print("# to return");
+}
+
+void showPasswordSetupScreen() {
+  lcd.print("Set Password");
+  lcd.setCursor(0, 1);
+  lcd.print("#=Redo *=Confirm");
+}
+
+void showAdminSetupScreen() {
+  lcd.print("Set PIN");
   lcd.setCursor(0, 1);
   lcd.print("#=Redo *=Confirm");
 }
@@ -45,7 +60,93 @@ void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
 
-  showSetupScreen();
+  showAdminSetupScreen();
+}
+
+void setPIN(char key) {
+  if (key == '#') {
+    current = "";
+    lcd.clear();
+    showAdminSetupScreen();
+  } else if (key == '*') {
+    isSetPIN = false;
+    pin = current;
+
+    current = "";
+    lcd.clear();
+    showPasswordSetupScreen();
+  }
+}
+
+void setPassword(char key) {
+  if (key == '#') {
+    current = "";
+    lcd.clear();
+    showPasswordSetupScreen();
+  } else if (key == '*') {
+    isSetPassword = false;
+    password = current;
+
+    current = "";
+    lcd.clear();
+    showEnterPasswordScreen();
+  }
+}
+
+void enterPassword(char key) {
+  if (current.length() >= password.length()) {
+    lcd.clear();
+
+    if (current == password) {
+      numberOfTries = 1;
+      current = "";
+      lcd.print("Unlock");
+      lcd.setCursor(0, 1);
+      lcd.print("Successful");
+    } else if (numberOfTries < NUMBER_OF_TRIES_MAX) {
+      numberOfTries ++;
+      current = "";
+      lcd.print("Wrong Password");
+    } else {
+      numberOfTries = 1;
+      current = "";
+
+      lcd.print("Access Denied");
+      lcd.setCursor(0, 1);
+      String message = "Blocked for # seconds";
+      message.replace("#", String(TIMEOUT));
+      lcd.print(message);
+      delay(1000 * TIMEOUT);
+
+      lcd.clear();
+      showEnterPasswordScreen();
+    }
+  }
+}
+
+void enterPIN(char key) {
+  if (key == '#') {
+    isEnterPIN = false;
+    showEnterPasswordScreen();
+  }
+
+  if (current.length() >= pin.length()) {
+    lcd.clear();
+
+    if (current == pin) {
+      current = "";
+      isSetPassword = true;
+      isEnterPIN = false;
+      showPasswordSetupScreen();
+    } else {
+      current = "";
+      isEnterPIN = false;
+      lcd.print("Wrong PIN");
+      delay(500);
+      lcd.clear();
+      showEnterPasswordScreen();
+    }
+  }
 }
 
 void processInput(char key) {
@@ -58,46 +159,17 @@ void processInput(char key) {
     current += String(key);
   }
 
-  if (isFirstTime) {
-    if (key == '#') {
-      current = "";
-      lcd.clear();
-      showSetupScreen();
-    } else if (key == '*') {
-      isFirstTime = false;
-      password = current;
-      current = "";
-      lcd.clear();
-      showDefaultScreen();
-    }
+  if (isSetPIN) {
+    setPIN(key);
+  } else if (isSetPassword) {
+    setPassword(key);
+  } else if (isEnterPIN) {
+    enterPIN(key);
+  } else if (key == '*') {
+    isEnterPIN = true;
+    showEnterPINScreen();
   } else {
-    if (current.length() >= password.length()) {
-      lcd.clear();
-      if (current == password) {
-        numberOfTries = 1;
-        current = "";
-        lcd.print("Unlock");
-        lcd.setCursor(0, 1);
-        lcd.print("Successful");
-      } else if (numberOfTries < NUMBER_OF_TRIES_MAX) {
-        numberOfTries ++;
-        current = "";
-        lcd.print("Wrong Password");
-      } else {
-        numberOfTries = 1;
-        current = "";
-
-        lcd.print("Access Denied");
-        lcd.setCursor(0, 1);
-        String message = "Blocked for # seconds";
-        message.replace("#", String(TIMEOUT));
-        lcd.print(message);
-        delay(1000 * TIMEOUT);
-
-        lcd.clear();
-        showDefaultScreen();
-      }
-    }
+    enterPassword(key);
   }
 }
 
